95d35767fcef7eb23938632ac7bae798579218e2
Joshrobertson023 <therealjoshrobertson@gmail.com>
2023-11-13 12:39:47 -0600

    Fixed minor bugs after comparing outputs
    Compared teacher's output with my output, used $ diff to compare
    differences

--- program7.c
+++ program7.c
@@ -49,17 +49,17 @@ void print_heading();
    /* Print the program heading                                       */
 void print_instructions();
    /* Print the program instructions                                  */
-void get_data(int array[], int last_index); /* Rename array to something meaningful */
+void get_data(int data[], int last_index); /* Rename array to something meaningful */
    /* Get the data for the array */
-void show_data(int array[], int last_index, int search_target);
+void show_data(int data[], int last_index, int search_target);
    /* */
 void search_result(char result, int target_location);
    /* */
-int sequential_search(int array[], int last_index, int search_target, int *p_target_location);
+int sequential_search(int data[], int last_index, int search_target, int *p_target_location);
    /* */
-int probability_search(int array[], int last_index, int search_target, int *p_target_location);
+int probability_search(int data[], int last_index, int search_target, int *p_target_location);
    /* */
-int binary_search(int array[], int last_index, int search_target, int *p_target_location);
+int binary_search(int data[], int last_index, int search_target, int *p_target_location);
    /* */
 
 /**********************************************************************/
@@ -113,7 +113,8 @@ int main()
    }
 
    /* Say goodbye and terminate the program */
-   printf("\n\nThanks for searching. Have a nice day! ;-)");
+   printf("\n\nThanks for searching.  Have a nice day! ;-)");
+   printf("\n\n\n\n\n\n");
    return 0;
 }
 
@@ -123,11 +124,11 @@ int main()
 void print_heading()
 {
    printf("\n\n\n\n\n");
-   printf("\n========================================================");
-   printf("\n              Program Number: %d",    PROGRAM_NUMBER);
-   printf("\n              Programmer: %s",        PROGRAMMER_NAME);
-   printf("\n              PCC Course Number: %s", COURSE_NUMBER);
-   printf("\n========================================================");
+   printf("\n======================================================");
+   printf("\n               Program Number: %d",    PROGRAM_NUMBER);
+   printf("\n               Programmer: %s",        PROGRAMMER_NAME);
+   printf("\n               PCC Course Number: %s", COURSE_NUMBER);
+   printf("\n======================================================");
    return;
 }
 
@@ -136,11 +137,11 @@ void print_heading()
 /**********************************************************************/
 void print_instructions()
 {
-   printf("\nThis program demonstrates various search algorithms");
+   printf("\nThis program demonstrates various search algorithms.");
    printf("\nYou enter in any whole number, and the program will");
    printf("\nsearch for it in an ORDERED array of whole numbers");
    printf("\nusing each of the following search algorithms:");
-   printf("\n     1. Ordered Sequentail Serach");
+   printf("\n     1. Ordered Sequential Search");
    printf("\n     2. Probability Search");
    printf("\n     3. Binary Search");
    printf("\nThe progress of each search is shown so the efficiency");
@@ -151,12 +152,12 @@ void print_instructions()
 /**********************************************************************/
 /*                                     */
 /**********************************************************************/
-void get_data(int array[], int last_index)
+void get_data(int data[], int last_index)
 {
    int array_index; /* */
 
    for(array_index = 0; array_index <= last_index; array_index++)
-      array[array_index] = (array_index * 5) + 10;
+      data[array_index] = (array_index * 5) + 10;
 
    return;
 }
@@ -164,7 +165,7 @@ void get_data(int array[], int last_index)
 /**********************************************************************/
 /*                                     */
 /**********************************************************************/
-void show_data(int array[], int last_index, int search_target)
+void show_data(int data[], int last_index, int search_target)
 {
    int array_index; /* */
 
@@ -174,9 +175,9 @@ void show_data(int array[], int last_index, int search_target)
 
    printf("\n    Array Data: ");
    for(array_index = 0; array_index <= last_index; array_index++)
-      printf(" %2d ", array[array_index]);
+      printf(" %2d ", data[array_index]);
 
-   printf("\n   User Target:  %d", search_target);
+   printf("\n   User Target:  %2d", search_target);
 
    return;
 }
@@ -186,7 +187,7 @@ void show_data(int array[], int last_index, int search_target)
 /**********************************************************************/
 void search_result(char result, int target_location)
 {
-   printf("\nSearch outcome: ");
+   printf("\nSearch Outcome: ");
 
    if(result == 'S')
       printf("Successful - target found at index [%2d]", target_location);
@@ -202,15 +203,15 @@ void search_result(char result, int target_location)
 /**********************************************************************/
 /*                                     */
 /**********************************************************************/
-int sequential_search(int array[], int last_index, int search_target, int *p_target_location)
+int sequential_search(int data[], int last_index, int search_target, int *p_target_location)
 {
    int search_index = 0; /* */
 
    printf("\n   Search Path: ");
 
-   if(search_target < array[last_index])
+   if(search_target <= data[last_index])
    {
-      while(search_index <= last_index && search_target > array[search_index])
+      while(search_index <= last_index && search_target > data[search_index])
       {
          printf("[%2d]", search_index);
          search_index++;
@@ -224,20 +225,20 @@ int sequential_search(int array[], int last_index, int search_target, int *p_tar
       *p_target_location = last_index;
    }
 
-   return(search_target == array[*p_target_location]);
+   return(search_target == data[*p_target_location]);
 }
 
 /**********************************************************************/
 /*                                     */
 /**********************************************************************/
-int probability_search(int array[], int last_index, int search_target, int *p_target_location)
+int probability_search(int data[], int last_index, int search_target, int *p_target_location)
 {
    int search_index = 0, /* */
        temporary;        /* */ /* Rename! */
 
    printf("\n   Search Path: ");
 
-   while(search_index < last_index && search_target != array[search_index])
+   while(search_index < last_index && search_target != data[search_index])
    {
       printf("[%2d]", search_index);
       search_index++;
@@ -247,19 +248,19 @@ int probability_search(int array[], int last_index, int search_target, int *p_ta
 
    if(search_index > 0)
    {
-      temporary = array[search_index];
-      array[search_index] = array[search_index - 1];
-      array[search_index - 1] = temporary;
+      temporary = data[search_index];
+      data[search_index] = data[search_index - 1];
+      data[search_index - 1] = temporary;
       *p_target_location -= 1;
    }
 
-   return(search_target == array[*p_target_location]);
+   return(search_target == data[*p_target_location]);
 }
 
 /**********************************************************************/
 /*                                     */
 /**********************************************************************/
-int binary_search(int array[], int last_index, int search_target, int *p_target_location)
+int binary_search(int data[], int last_index, int search_target, int *p_target_location)
 {
    int beginning_index = 0,    /* */
        middle_index = 0,       /* */
@@ -272,15 +273,15 @@ int binary_search(int array[], int last_index, int search_target, int *p_target_
       middle_index = (beginning_index + end_index) / 2;
       printf("[%2d]", middle_index);
 
-      if(search_target > array[middle_index])
+      if(search_target > data[middle_index])
          beginning_index = (middle_index + 1);
       else
-         if(search_target < array[middle_index])
+         if(search_target < data[middle_index])
             end_index = middle_index - 1;
          else
             beginning_index = (end_index + 1);
    }
    *p_target_location = middle_index;
 
-   return (search_target == array[*p_target_location]);
+   return (search_target == data[*p_target_location]);
 }
