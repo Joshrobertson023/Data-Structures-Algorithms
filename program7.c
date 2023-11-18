9bdb84ee2ce9353f0db387a40dbbc2acd983b0c9
Joshrobertson023 <therealjoshrobertson@gmail.com>
2023-11-18 11:38:56 -0600

    Final submission

--- program7.c
+++ program7.c
@@ -1,6 +1,6 @@
 /**********************************************************************/
 /*                                                                    */
-/* Program Name: program7 - Demonstrating Various Search Algorithms   */
+/* Program Name: program7 - Demonstrate Various Search Algorithms     */
 /* Author:       Josh Robertson                                       */
 /* Installation: Pensacola Christian College, Pensacola, Florida      */
 /* Course:       CS227, Data Structures and Algorithms                */
@@ -23,10 +23,17 @@
 /*                                                                    */
 /**********************************************************************/
 
-/* TO-DO */
-/* Rename array[] to something meaningful throught program */
-
 /**********************************************************************/
+/*                                                                    */
+/* This program asks for a whole number, then searches through three  */
+/* whole number arrays with three different searching algorithms:     */
+/*    1. Sequential Search                                            */
+/*    2. Probability Search                                           */
+/*    3. Binary Search                                                */
+/* For each searching algorithm, the program displays the whole       */
+/* numbers with their corresponding index, the number the user        */
+/* searched for, the search path taken by the algorithm, and whether  */
+/* or not the search was successful.                                  */
 /*                                                                    */
 /**********************************************************************/
 
@@ -38,9 +45,9 @@
 #define COURSE_NUMBER   "CS227"       /* PCC assigned course number   */
 #define PROGRAMMER_NAME "Robertson"   /* The programmer's last name   */
 #define PROGRAM_NUMBER  7             /* Teacher assigned program num */
-#define DATA_SIZE       15            /* */
-#define LAST_INDEX      DATA_SIZE - 1 /* */
-#define QUIT            0             /* */
+#define DATA_SIZE       15            /* Size of the integer arrays   */
+#define LAST_INDEX      DATA_SIZE - 1 /* Last index of the arrays     */
+#define QUIT            0             /* Program exit value           */
 
 /**********************************************************************/
 /*                        Function Prototypes                         */
@@ -49,71 +56,83 @@ void print_heading();
    /* Print the program heading                                       */
 void print_instructions();
    /* Print the program instructions                                  */
-void get_data(int data[], int last_index); /* Rename array to something meaningful */
-   /* Get the data for the array */
-void show_data(int data[], int last_index, int search_target);
-   /* */
+void get_data(int whole_number_data[], int last_index);
+   /* Get the whole number data for the array                         */
+void show_data(int whole_number_data[], int last_index,
+               int search_target);
+   /* Print the array indexes, array data, and user search target     */
 void search_result(char result, int target_location);
-   /* */
-int sequential_search(int data[], int last_index, int search_target, int *p_target_location);
-   /* */
-int probability_search(int data[], int last_index, int search_target, int *p_target_location);
-   /* */
-int binary_search(int data[], int last_index, int search_target, int *p_target_location);
-   /* */
+   /* Print the search result                                         */
+int sequential_search(int seq_data[], int last_index, int search_target,
+                      int *p_target_location);
+   /* Search whole numbers using a sequential search algorithm        */
+int probability_search(int prb_data[], int last_index, int search_target,
+                       int *p_target_location);
+   /* Search whole numbers using a probability  search algorithm      */
+int binary_search(int bin_data[], int last_index, int search_target,
+                  int *p_target_location);
+   /* Search whole numbers using a binary search algorithm            */
 
 /**********************************************************************/
 /*                           Main Function                            */
 /**********************************************************************/
 int main() 
 {
-   int seq_data[DATA_SIZE], /* */
-       prb_data[DATA_SIZE], /* */
-       bin_data[DATA_SIZE], /* */
-       search_target,       /* */
-       target_location;     /* */
+   int seq_data[DATA_SIZE], /* Whole number data for the sequential   */
+                            /* search                                 */
+       prb_data[DATA_SIZE], /* Whole number data for the probability  */
+                            /* search                                 */
+       bin_data[DATA_SIZE], /* Whole number data for the binary       */
+                            /* search                                 */
+       search_target,       /* User's whole number search target      */
+       target_location;     /* Search target's location in the data   */
 
    /* Print the program heading and instructions                      */
-   printf("\n\n\n\n\n\n\n");
    print_heading();
    print_instructions();
 
-   /* Get the data for each search algorithm */
+   /* Get the whole number data for each search algorithm             */
    get_data(seq_data, LAST_INDEX);
    get_data(prb_data, LAST_INDEX);
    get_data(bin_data, LAST_INDEX);
    
-   /* Loop processing ... until user quits */
-   while(printf("\n\n\nEnter an integer search target (0 to quit): "), 
+   /* Loop processing searching for whole numbers until user quits    */
+   while(printf("\n\n\nEnter an integer search target (0 to quit): "),
       scanf("%d", &search_target), (search_target != QUIT))
    {
-      /* Ordered Sequential Search */
+      /* Show the whole number data, search for the target using an   */
+      /* Ordered Sequential Search, and print the search outcome      */
       printf("\n\nOrdered Sequential Search:");
       show_data(seq_data, LAST_INDEX, search_target);
-      if(sequential_search(seq_data, LAST_INDEX, search_target, &target_location))
+      if(sequential_search(seq_data, LAST_INDEX, 
+                           search_target, &target_location))
          search_result('S', target_location);
       else 
          search_result('U', target_location);
 
-      /* Probability search */
+      /* Show the whole number data, search for the target using a    */
+      /* Probability Search, and print the search outcome             */
       printf("\n\nProbability Search:");
       show_data(prb_data, LAST_INDEX, search_target);
-      if(probability_search(prb_data, LAST_INDEX, search_target, &target_location))
+      if(probability_search(prb_data, LAST_INDEX, 
+                            search_target, &target_location))
          search_result('S', target_location);
       else 
          search_result('U', target_location);
 
-      /* Binary search */
+      /* Show the whole number data, search for the target using a    */
+      /* Binary Search, and print the search outcome                  */
       printf("\n\nBinary Search:");
       show_data(bin_data, LAST_INDEX, search_target);
-      if(binary_search(bin_data, LAST_INDEX, search_target, &target_location))
+      if(binary_search(bin_data, LAST_INDEX, 
+                       search_target, &target_location))
          search_result('S', target_location);
       else
          search_result('U', target_location);
    }
 
-   /* Say goodbye and terminate the program */
-   printf("\n\nThanks for searching.  Have a nice day! ;-)");
+   /* Say goodbye and terminate the program                           */
+   printf("\nThanks for searching.  Have a nice day! ;-)");
    printf("\n\n\n\n\n\n");
    return 0;
 }
@@ -123,7 +142,7 @@ int main()
 /**********************************************************************/
 void print_heading()
 {
-   printf("\n\n\n\n\n");
+   printf("\n\n\n\n\n\n");
    printf("\n======================================================");
    printf("\n               Program Number: %d",    PROGRAM_NUMBER);
    printf("\n               Programmer: %s",        PROGRAMMER_NAME);
@@ -150,47 +169,53 @@ void print_instructions()
 }
 
 /**********************************************************************/
-/*                                     */
+/*               Get the whole number data for the array              */
 /**********************************************************************/
-void get_data(int data[], int last_index)
+void get_data(int whole_number_data[], int last_index)
 {
-   int array_index; /* */
+   int array_index; /* Index of every whole number                    */
 
    for(array_index = 0; array_index <= last_index; array_index++)
-      data[array_index] = (array_index * 5) + 10;
+      whole_number_data[array_index] = (array_index * 5) + 10;
 
    return;
 }
 
 /**********************************************************************/
-/*                                     */
+/*     Print the array indexes, array data, and user search target    */
 /**********************************************************************/
-void show_data(int data[], int last_index, int search_target)
+void show_data(int whole_number_data[], int last_index,
+               int search_target)
 {
-   int array_index; /* */
+   int array_index; /* Index of every whole number                    */
 
-   printf("\n   Array Index: ");
+   printf      ("\n   Array Index: ");
    for(array_index = 0; array_index <= last_index; array_index++)
-      printf("[%2d]", array_index);
+      printf   ("[%2d]", array_index);
 
-   printf("\n    Array Data: ");
+   printf      ("\n    Array Data: ");
    for(array_index = 0; array_index <= last_index; array_index++)
-      printf(" %2d ", data[array_index]);
+   {
+      printf   (" %2d", whole_number_data[array_index]);
+      if(array_index < last_index)
+         printf(" ");
+   }
 
-   printf("\n   User Target:  %2d", search_target);
+   printf      ("\n   User Target:  %2d", search_target);
 
    return;
 }
 
 /**********************************************************************/
-/*                                     */
+/*                       Print the search result                      */
 /**********************************************************************/
 void search_result(char result, int target_location)
 {
    printf("\nSearch Outcome: ");
 
    if(result == 'S')
-      printf("Successful - target found at index [%2d]", target_location);
+      printf   ("Successful - target found at index [%2d]",
+         target_location);
    else
       if(result == 'U')
          printf("Unsuccessful - target not found");
@@ -201,70 +226,75 @@ void search_result(char result, int target_location)
 }
 
 /**********************************************************************/
-/*                                     */
+/*    Search for a whole number using a sequential search algorithm   */
 /**********************************************************************/
-int sequential_search(int data[], int last_index, int search_target, int *p_target_location)
+int sequential_search(int seq_data[], int last_index,
+                      int search_target, int *p_target_location)
 {
-   int search_index = 0; /* */
+   int search_index = 0; /* Index searching through every whole number */
 
    printf("\n   Search Path: ");
 
-   if(search_target <= data[last_index])
+   if(search_target <= seq_data[last_index])
    {
-      while(search_index <= last_index && search_target > data[search_index])
+      while(search_index <= last_index && 
+            search_target > seq_data[search_index])
       {
          printf("[%2d]", search_index);
          search_index++;
       }
-      printf("[%2d]", search_index);
+      printf   ("[%2d]", search_index);
       *p_target_location = search_index;
    }
    else
    {
-      printf("[%2d]", last_index);
+      printf   ("[%2d]", last_index);
       *p_target_location = last_index;
    }
 
-   return(search_target == data[*p_target_location]);
+   return(search_target == seq_data[*p_target_location]);
 }
 
 /**********************************************************************/
-/*                                     */
+/*    Search for a whole number using a probability search algorithm  */
 /**********************************************************************/
-int probability_search(int data[], int last_index, int search_target, int *p_target_location)
+int probability_search(int prb_data[], int last_index, 
+                       int search_target, int *p_target_location)
 {
-   int search_index = 0, /* */
-       temporary;        /* */ /* Rename! */
+   int search_index = 0, /* Index searching through every whole number */
+       temporary_number; /* Temporary number used to swap two numbers  */
 
    printf("\n   Search Path: ");
 
-   while(search_index < last_index && search_target != data[search_index])
+   while(search_index < last_index && 
+         search_target != prb_data[search_index])
    {
       printf("[%2d]", search_index);
       search_index++;
    }
-   printf("[%2d]", search_index);
+   printf   ("[%2d]", search_index);
    *p_target_location = search_index;
 
-   if(search_index > 0)
+   if(search_index > 0 && search_index < LAST_INDEX)
    {
-      temporary = data[search_index];
-      data[search_index] = data[search_index - 1];
-      data[search_index - 1] = temporary;
+      temporary_number = prb_data[search_index];
+      prb_data[search_index] = prb_data[search_index - 1];
+      prb_data[search_index - 1] = temporary_number;
       *p_target_location -= 1;
    }
 
-   return(search_target == data[*p_target_location]);
+   return(search_target == prb_data[*p_target_location]);
 }
 
 /**********************************************************************/
-/*                                     */
+/*      Search for a whole number using a binary search algorithm     */
 /**********************************************************************/
-int binary_search(int data[], int last_index, int search_target, int *p_target_location)
+int binary_search(int bin_data[], int last_index,
+                  int search_target, int *p_target_location)
 {
-   int beginning_index = 0,    /* */
-       middle_index = 0,       /* */
-       end_index = last_index; /* */
+   int beginning_index = 0,    /* Start of the data being searched    */
+       middle_index = 0,       /* Middle of the data being searched   */
+       end_index = last_index; /* End of the data being searched      */
 
    printf("\n   Search Path: ");
 
@@ -273,15 +303,15 @@ int binary_search(int data[], int last_index, int search_target, int *p_target_l
       middle_index = (beginning_index + end_index) / 2;
       printf("[%2d]", middle_index);
 
-      if(search_target > data[middle_index])
+      if(search_target > bin_data[middle_index])
          beginning_index = (middle_index + 1);
       else
-         if(search_target < data[middle_index])
+         if(search_target < bin_data[middle_index])
             end_index = middle_index - 1;
          else
             beginning_index = (end_index + 1);
    }
    *p_target_location = middle_index;
 
-   return (search_target == data[*p_target_location]);
+   return (search_target == bin_data[*p_target_location]);
 }
